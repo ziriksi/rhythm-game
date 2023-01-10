2fecbc93a9bf14bdee56f37857e3beb6daea85da
ziriksi <78670605+ziriksi@users.noreply.github.com>
2023-01-10 16:17:09 +0000

    c

--- scripts/download-string.js
+++ scripts/download-string.js
@@ -1,7 +1,7 @@
 // Credit: https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
 
 export default function download(filename, text) {
-  var element = document.createElement('a');
+  const element = document.createElement('a');
   element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
   element.setAttribute('download', filename);
 
